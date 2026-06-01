5f5f2a57884761df6cb97eb302714d6906301cae
I578071 <suvabrata.chowdhury@sap.com>
2026-06-01 22:08:28 +0530

    Refactorings

---
+++ src/base/shape.ts
@@ -0,0 +1,48 @@
+import p5 from 'p5';
+
+export interface ShapeProps {
+    p: p5,
+    isHighlighted?: boolean,
+    isHidden?: boolean,
+};
+
+export abstract class Shape {
+    readonly p: p5;
+    private _isHighlighted: boolean;
+    private _isHidden: boolean;
+
+    constructor({p, isHighlighted = false, isHidden = false}: ShapeProps) {
+        this.p = p;
+        this._isHighlighted = isHighlighted;
+        this._isHidden = isHidden;
+    }
+
+    get isHighlighted(): boolean {
+        return this._isHighlighted;
+    }
+
+    set isHighlighted(isHighlighted: boolean) {
+        this._isHighlighted = isHighlighted;
+
+        this.conditionalDraw();
+    }
+
+    get isHidden(): boolean {
+        return this._isHidden;
+    }
+
+    set isHidden(isHidden: boolean) {
+        this._isHidden = isHidden;
+
+        this.conditionalDraw();
+    }
+
+    public conditionalDraw = () => {
+        if (!this._isHidden) {
+            this.draw();
+        }
+    }
+
+    abstract draw(): void;
+}
+
