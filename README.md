668948ebd31b1fb6452dffda6c94bdbf8426b02c
I578071 <suvabrata.chowdhury@sap.com>
2026-06-09 20:48:28 +0530

    init concept

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # p5.viz
 
 A p5.js library for visualization stuff written in TypeScript.
+
+
+## Example calls
+
+```js
+
+frames = [];
+
+queueFrame(method) {
+    frames.push(method);
+}
+
+frame() {
+    for frame_desc of frames {
+        frame_desc();
+    }
+}
+
+setup() {
+    node = node(pos, size, txt);
+
+    queueFrame(highlight(node), style, [timing]);
+}
+
+draw() {
+    frame(frameCount);
+}
+
+// 60 FPS by default
+while(true) {
+    draw();
+}
+```
+
+## Design
+
+```mermaid
+flowchart TD
+
+    frames[(Callbacks for Frames)]
+
+    subgraph setup
+        setup_start --> queueFrames
+        queueFrames --> setup_end
+        queueFrames <--> frames
+    end
+
+
+    subgraph draw
+        setup_end --> draw_start
+        draw_start --> frame
+        frame --> draw_end
+        frame <--> frames
+        draw_end --> draw_start
+    end
+```
--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,48 @@ import p5 from 'p5';
 
 import './style.css';
 
+let animations = [];
+
+function frame() {
+  for(const animation of animations) {
+    animation();
+  }
+};
+
+function queueFrame(frame) {
+  animations.push(frame);
+}
+
 // Define the sketch using a p5 instance parameter
 const sketch = (p: p5) => {
-  let x = 0;
-  let y = 0;
+  // let x = 0;
+  // let y = 0;
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(220);
+    // p.background(220);
+    
+    // x = p.windowWidth/2;
+    // y = p.windowHeight/2;
+
+    let frameOffset = 0;
+    queueFrame(() => {
+
+      const currentFrame = p.frameCount - frameOffset;
+
+      if(p.mouseIsPressed) {
+        frameOffset = p.frameCount;
+      }
+
+      p.circle(p.mouseX, p.mouseY, Math.min(200, currentFrame));
+    });
   };
 
   p.draw = () => {
     p.background(220);
     p.fill(255, 0, 0);
-
-    p.circle(x, y, 50);
-
-    // Simple animation
-    x = (x + 1) % p.windowWidth;
-    y = (y + 1) % p.windowHeight;
+    
+    frame();
   };
 
   p.windowResized = () => {
