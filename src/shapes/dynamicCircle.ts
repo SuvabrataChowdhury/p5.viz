e65a4a14ac90ad460e1a152e30253233fc92721e
Suvabrata Chowdhury <114484620+SuvabrataChowdhury@users.noreply.github.com>
2026-06-28 18:41:29 +0530

    Initial functional library (#3)
    * init concept
    
    * animation concept
    
    * design for implementation
    
    * init framework
    
    * Animation proposal
    
    * animation init impl
    
    * linting fixes
    
    * some more animations
    
    * Refactorings
    
    ---------
    
    Co-authored-by: I578071 <suvabrata.chowdhury@sap.com>

--- src/shapes/dynamicCircle.ts
+++ src/shapes/dynamicCircle.ts
@@ -1,5 +1,5 @@
 import p5 from 'p5';
-import type { AnimationParams, DynamicShape, P5Callback, ShapeAnimation, ShapeParams } from "../core";
+import type { AnimationParams, DynamicShape, P5Callback, ShapeParams } from "../core";
 
 interface CircleParams extends ShapeParams {
     x: number,
@@ -7,27 +7,29 @@ interface CircleParams extends ShapeParams {
     d: number
 };
 
-const growCircle: ShapeAnimation<CircleParams> = (shapeParams: CircleParams, currentFrame: number): CircleParams => {
-
-    // const currentRelativeFrame = p.frameCount - animationParams.firstFrameCount;
-
+const growAnimation: AnimationParams<CircleParams>['animate'] = (p: p5, shapeParams: CircleParams, currentFrame: number) => {
     const { x, y, d } = shapeParams;
-
-    return {
-        x,
-        y,
-        d: Math.min(d, currentFrame) 
-    };
+    p.circle(x, y, Math.min(d, currentFrame));
 }
 
-const dynamicCircle: DynamicShape<CircleParams> = (shapeParams: CircleParams, animationParams: AnimationParams<CircleParams>): P5Callback => {
-    return (p: p5) => {
-        const currentRelativeFrame = p.frameCount - animationParams.firstFrameCount;
+const drawAnimation: AnimationParams<CircleParams>['animate'] = (p: p5, shapeParams: CircleParams, currentFrame: number) => {
+    const { x, y, d } = shapeParams;
+    p.arc(x, y, d, d, 0, Math.min(2 * Math.PI, currentFrame * Math.PI / 180));
+}
 
-        const { x, y, d } = animationParams.shapeAnimation(shapeParams, currentRelativeFrame);
-
+const defaultCircleAnimation: AnimationParams<CircleParams> = {
+    animate: (p: p5, shapeParams: CircleParams) => {
+        const { x, y, d } = shapeParams;
         p.circle(x, y, d);
     }
 }
 
-export { dynamicCircle, growCircle, type CircleParams };
+const dynamicCircle: DynamicShape<CircleParams> = (shapeParams: CircleParams, animationParams: AnimationParams<CircleParams> = defaultCircleAnimation): P5Callback => {
+    let firstFrameCount: number | null = null;
+    return (p: p5) => {
+        if (firstFrameCount === null) firstFrameCount = p.frameCount;
+        animationParams.animate(p, shapeParams, p.frameCount - firstFrameCount);
+    }
+}
+
+export { dynamicCircle, growAnimation, drawAnimation,  type CircleParams };
